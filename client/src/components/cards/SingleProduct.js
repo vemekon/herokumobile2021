e0865697a11632664fccca87c4ef9353e397eea5
samuel112 <senaymokie@gmail.com>
2020-12-29 21:26:14 +0000

    First commit of the project

---
+++ client/src/components/cards/SingleProduct.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import { Card, Tabs, Tooltip } from "antd";
+import { Link } from "react-router-dom";
+import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Laptop from "../../images/laptop.png";
+import ProductListItems from "./ProductListItems";
+import StarRating from "react-star-ratings";
+import RatingModal from "../modal/RatingModal";
+import { showAverage } from "../../functions/rating";
+import _ from "lodash";
+import { useSelector, useDispatch } from "react-redux";
+
+const { TabPane } = Tabs;
+
+// this is childrend component of Product page
+const SingleProduct = ({ product, onStarClick, star }) => {
+  const [tooltip, setTooltip] = useState("Click to add");
+
+  // redux
+  const { user, cart } = useSelector((state) => ({ ...state }));
+  const dispatch = useDispatch();
+
+  const { title, images, description, _id } = product;
+
+  const handleAddToCart = () => {
+    // create cart array
+    let cart = [];
+    if (typeof window !== "undefined") {
+      // if cart is in local storage GET it
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      // push new product to cart
+      cart.push({
+        ...product,
+        count: 1,
+      });
+      // remove duplicates
+      let unique = _.uniqWith(cart, _.isEqual);
+      // save to local storage
+      // console.log('unique', unique)
+      localStorage.setItem("cart", JSON.stringify(unique));
+      // show tooltip
+      setTooltip("Added");
+
+      // add to reeux state
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: unique,
+      });
+      // show cart items in side drawer
+      dispatch({
+        type: "SET_VISIBLE",
+        payload: true,
+      });
+    }
+  };
+
+  return (
+    <>
+      <div className="col-md-7">
+        {images && images.length ? (
+          <Carousel showArrows={true} autoPlay infiniteLoop>
+            {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+          </Carousel>
+        ) : (
+          <Card cover={<img src={Laptop} className="mb-3 card-image" />}></Card>
+        )}
+
+        <Tabs type="card">
+          <TabPane tab="Description" key="1">
+            {description && description}
+          </TabPane>
+          <TabPane tab="More" key="2">
+            Call use on xxxx xxx xxx to learn more about this product.
+          </TabPane>
+        </Tabs>
+      </div>
+
+      <div className="col-md-5">
+        <h1 className="bg-info p-3">{title}</h1>
+
+        {product && product.ratings && product.ratings.length > 0 ? (
+          showAverage(product)
+        ) : (
+          <div className="text-center pt-1 pb-3">No rating yet</div>
+        )}
+
+        <Card
+          actions={[
+            <Tooltip title={tooltip}>
+              <a onClick={handleAddToCart}>
+                <ShoppingCartOutlined className="text-danger" /> <br /> Add to
+                Cart
+              </a>
+            </Tooltip>,
+            <Link to="/">
+              <HeartOutlined className="text-info" /> <br /> Add to Wishlist
+            </Link>,
+            <RatingModal>
+              <StarRating
+                name={_id}
+                numberOfStars={5}
+                rating={star}
+                changeRating={onStarClick}
+                isSelectable={true}
+                starRatedColor="red"
+              />
+            </RatingModal>,
+          ]}
+        >
+          <ProductListItems product={product} />
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default SingleProduct;
