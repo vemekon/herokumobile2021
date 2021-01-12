fb1ba7a0b705fbc8f7b3b6ab61d8f3c32145a4e8
samuel112 <senaymokie@gmail.com>
2021-01-12 20:55:35 +0000

    m

--- server/server.js
+++ server/server.js
@@ -11,33 +11,30 @@ require("dotenv").config();
 // app
 const app = express();
 
-mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB CONNECTED"))
-  .catch((err) => console.log("DB CONNECTION ERR", err));
+// mongoose
+//   .connect(process.env.DATABASE, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("DB CONNECTED"))
+//   .catch((err) => console.log("DB CONNECTION ERR", err));
 
-// middlewares
-app.use(morgan("dev"));
-app.use(bodyParser.json({ limit: "2mb" }));
-app.use(cors());
+// app.use(morgan("dev"));
+// app.use(bodyParser.json({ limit: "2mb" }));
+// app.use(cors());
 
-const apath = path.resolve(__dirname, "../", "client/build");
+// const apath = path.resolve(__dirname, "../", "client/build");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(apath));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(apath));
+// }
 
-// routes middleware
-const routepath = path.resolve(__dirname, "routes");
-console.log(apath);
-readdirSync(routepath).map((r) => app.use("/api", require("./routes/" + r)));
+// const routepath = path.resolve(__dirname, "routes");
+// console.log(apath);
+// readdirSync(routepath).map((r) => app.use("/api", require("./routes/" + r)));
 
-// port
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
